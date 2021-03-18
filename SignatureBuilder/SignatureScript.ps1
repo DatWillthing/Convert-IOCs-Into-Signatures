6a0e8c8c6a3d437cf08f8e4881d65e8988ff8b7f
Michael Zalud <mazalud@gmail.com>
2021-03-17 14:01:28 -1000

    Update SignatureScript.ps1
    Up to data script with no functions yet...

--- SignatureBuilder/SignatureScript.ps1
+++ SignatureBuilder/SignatureScript.ps1
@@ -1,54 +1,56 @@
 Set-Variable -Name "File" -Value "$args" -Scope global
-Set-Variable -Name "TotalSigs" -Value "69696" -Scope global
-Set-Variable -Name "TotalHashes" -Value "42069" -Scope global
-Set-Variable -Name "TotalIPs" -Value "4200" -Scope global
-Set-Variable -Name "TotalSkips" -Value "69" -Scope global
+Set-Variable -Name "TotalSigs" -Value "0" -Scope global
+Set-Variable -Name "TotalHashes" -Value "0" -Scope global
+Set-Variable -Name "TotalIPs" -Value "0" -Scope global
+Set-Variable -Name "TotalSkips" -Value "0" -Scope global
 Set-Variable -Name "FirstSID" -Value "700000" -Scope global
-Set-Variable -Name "LastSID" -Value "8000000" -Scope global
-Set-Variable -Name "OutputName" -Value "poopy" -Scope global
+Set-Variable -Name "LastSID" -Value "700000" -Scope global
+Set-Variable -Name "OutputName" -Value "NotSureYet" -Scope global
 $FileExten=[System.IO.Path]::GetExtension("$File")
 $FileName=[System.IO.Path]::GetFileNameWithoutExtension("$File")
 $FilePath=[System.IO.Path]::GetDirectoryName("$File")
 $ResultsPath=Join-Path -Path $PSScriptRoot -ChildPath "Results"
 $LibsPath=Join-Path -Path $PSScriptRoot -ChildPath "Libs"
-$TempReadmePath=Join-Path -Path $LibsPath -ChildPath "TemplateReadme.txt"
+$TempReadmePath=Join-Path -Path $LibsPath -ChildPath "Temp-Readme.txt"
 $ReadmePath=Join-Path -Path $ResultsPath -ChildPath "Readme.txt"
 Function Detect-Filetype {
-	if ($FileExten -eq ".xlsx") {
-		write-host "Excel!!"
-		#Excel-File($File)
-	}
-	elseif ($FileExten -eq ".csv") {
-		write-host "CSV yes, please!!"
-		#Check-Headers
-	}
-	elseif ($FileExten -eq "") {
-		write-host "I did not detect a file extension..."
-	}
-	else {
-		write-host "Something didn't work..."
-	}
+    if ($FileExten -eq ".xlsx") {
+        write-host "Excel!!"
+        #Excel-File($File)
+    }
+    elseif ($FileExten -eq ".csv") {
+        write-host "CSV yes, please!!"
+        #Check-Headers
+    }
+    elseif ($FileExten -eq "") {
+        write-host "I did not detect a file extension..."
+    }
+    else {
+        write-host "Something didn't work..."
+    }
 }
 
 Function Excel-File ($File) {
+    #Ensure the naming scheme is right...
     $Excel = New-Object -ComObject Excel.Application
     $wb = $Excel.Workbooks.Open($File)
     foreach ($ws in $wb.Worksheets) {
         $ws.SaveAs($FilePath + "\" + $FileName + ".csv", 6)
-		$File = "$FilePath" + "\" + "$FileName" + ".csv"
-		write-host="$File"
+    ###Use a Join-Path here if possible...
+       $File = "$FilePath" + "\" + "$FileName" + ".csv"
+       write-host="$File"
     }
     $Excel.Quit()
-	write-host "`$File = $File"
-	Detect-Filetype
+    write-host "`$File = $File"
+    Detect-Filetype
 }
 
 Function Build-Results {
-	New-Item -Name "Results" -ItemType Directory -Path $PSScriptRoot
-	#Edit permissions?
-	New-Item -Name "Readme.txt" -ItemType File -Path $ResultsPath
-	$TemplateContent = get-content -path $TempReadmePath
-	Invoke-expression """$TemplateContent""" | set-content -path $ReadmePath
+    New-Item -Name "Results" -ItemType Directory -Path $PSScriptRoot
+    #Edit permissions?
+    New-Item -Name "Readme.txt" -ItemType File -Path $ResultsPath
+    $TemplateContent = get-content -path $TempReadmePath
+    Invoke-expression """$TemplateContent""" | set-content -path $ReadmePath
 }
 #Detect-Filetype
 Build-Results
