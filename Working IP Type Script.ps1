523956e088e8ad6c6793de3970b710c9080bfede
DatWillthing <79482819+DatWillthing@users.noreply.github.com>
2021-03-23 01:01:58 -0600

    Update Working IP Type Script.ps1

--- Working IP Type Script.ps1
+++ Working IP Type Script.ps1
@@ -1,3 +1,29 @@
+##Roadmap for the working script, use as a framework to build upon##
+
+##Import functions module##
+Import-Module -force -name 'C:\users\William\Desktop\Signature Script\SignatureModule.psm1'
+
+$filepath = 'C:\users\William\Desktop\Signature Script\test.csv' #read-host "Input the full path to the signature file: "
+$file = import-csv $filepath 
+
+##Convert invalid filetypes to csv##
+#Detect-Filetype
+
+##Variable Declaration##
+$count = 0
+$length = $file.length
+$savedrules = 'C:\Users\William\Desktop\Signature Script'
+
+##Counters##
+$SID = 1000000
+$TotalSigs = 0
+$TotalHashes = 0
+$TotalIPs = 0
+$TotalSkips = 0
+$FirstSID = 700000
+$LastSID = 700000
+$OutputName = "NotSureYet"
+
 function Detect-Type{
        
   if ($file[$count].type -eq "ip-src"){
@@ -44,4 +70,13 @@ function Detect-Type{
     echo "alert $IPSRC ANY <> $IPDST ANY (msg:`"$Message`"; content:`"$Content`"; reference:$Reference; classtype:$Classtype; sid:$SID; rev:1;)" `n  >> $savedrules\rules.txt
    }
    
+}
+
+##Iterates through the file and writes pertinent info to suricata rules##
+while ($count -lt $length){
+ 
+  Detect-Type
+  
+  $SID += 10
+  $count += 1
 }
