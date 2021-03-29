0dee8aa42cc09861ef07a64d00f41eb8c96720fc
Michael Zalud <mazalud@gmail.com>
2021-03-29 13:39:14 -1000

    Update SignatureScript.ps1

--- SignatureBuilder/SignatureScript.ps1
+++ SignatureBuilder/SignatureScript.ps1
@@ -1,67 +1,176 @@
+##Initial definitions for the Results Readme
 Set-Variable -Name "File" -Value "$args" -Scope global
+Set-Variable -Name "TotalSigs" -Value 0 -Scope global
+Set-Variable -Name "TotalHashes" -Value 0 -Scope global
+Set-Variable -Name "TotalIPs" -Value 0 -Scope global
+Set-Variable -Name "TotalSkips" -Value 0 -Scope global
+Set-Variable -Name "FirstSID" -Value 700000 -Scope global
+Set-Variable -Name "LastSID" -Value 0 -Scope global
+$global:SID = $FirstSID
+$Today=Get-Date -Format "yyyy-MM-dd"
 
-<#
-if ($File -eq "") {
-    $File = read-host "Input the full path to the signature file: "
-}
-
-#>
-
-Set-Variable -Name "TotalSigs" -Value "0" -Scope global
-Set-Variable -Name "TotalHashes" -Value "0" -Scope global
-Set-Variable -Name "TotalIPs" -Value "0" -Scope global
-Set-Variable -Name "TotalSkips" -Value "0" -Scope global
-Set-Variable -Name "FirstSID" -Value "700000" -Scope global
-Set-Variable -Name "LastSID" -Value "700000" -Scope global
-Set-Variable -Name "OutputName" -Value "NotSureYet" -Scope global
-$global:FileExten=[System.IO.Path]::GetExtension("$File")
-$global:FileName=[System.IO.Path]::GetFileNameWithoutExtension("$File")
-$global:FilePath=[System.IO.Path]::GetDirectoryName("$File")
+##Define localized paths
+New-Item -Name "Results" -ItemType Directory -Path $PSScriptRoot
 $global:ResultsPath=Join-Path -Path $PSScriptRoot -ChildPath "Results"
 $global:LibsPath=Join-Path -Path $PSScriptRoot -ChildPath "Libs"
 $global:TempReadmePath=Join-Path -Path $LibsPath -ChildPath "TemplateReadme.txt"
 $global:ReadmePath=Join-Path -Path $ResultsPath -ChildPath "Readme.txt"
-$global:ModulePath=Join-Path -Path $ResultsPath -ChildPath "SignatureModule.psm1"
-Function Detect-Filetype {
-    if ($FileExten -eq ".xlsx") {
-        write-host "Excel!!"
-        #Excel-File($File)
+$global:ModulePath=Join-Path -Path $LibsPath -ChildPath "Add-Features.psm1"
+New-Item -Name "$OutputName`.rules" -ItemType File -Path $ResultsPath
+
+##Import functions module##
+Import-Module -force -name "$ModulePath"
+
+Input-File
+Detect-Filetype
+
+$FileContent = import-csv -Path "$File" 
+
+##Variable Declaration##
+$Count = 0
+$length = $FileContent.length
+
+function Detect-Type {
+  if ($FileContent[$Count].type -eq "ip-src"){
+    $IPSRC = "ANY" 
+    $IPDST = "ANY"
+    $Type = @()
+    $Content = @()
+    $Message = @()
+    $Type += $FileContent[$Count].type
+    $Content += $FileContent[$Count].value
+    $Message += $FileContent[$Count].type 
+    $Reference += $FileContent[$Count].attribute_tag
+    $IPSRC = $Content
+    echo "alert $IPSRC ANY <> $IPDST ANY (msg:`"$Message`"; content:`"$Content`"; reference:$Reference; classtype:$Classtype; sid:$SID; rev:1;)" `n  >> $OutputFile`.rules
+    $global:SID += 1
+    $global:TotalIPs += 1
+   }
+   if ($FileContent[$Count].type -eq "ip-dst"){
+    $IPSRC = "ANY" 
+    $IPDST = "ANY"
+    $Type = @()
+    $Content = @()
+    $Message = @()
+    $Type += $FileContent[$Count].type
+    $Content += $FileContent[$Count].value
+    $Message += $FileContent[$Count].type 
+    $Reference += $FileContent[$Count].attribute_tag
+    $IPDST = $Content
+    echo "alert $IPSRC ANY <> $IPDST ANY (msg:`"$Message`"; content:`"$Content`"; reference:$Reference; classtype:$Classtype; sid:$SID; rev:1;)" `n  >> $OutputFile`.rules
+    $global:SID += 1
+    $global:TotalIPs += 1
+   }
+
+    ##Hash Statements
+
+   if ($FileContent[$Count].type -eq "sha1"){
+    $Type = @()
+    $Content = @()
+    $Message = @()
+    $Type += $FileContent[$Count].type
+    $Content += $FileContent[$Count].value
+    $Message += $FileContent[$Count].type 
+    $Reference += $FileContent[$Count].attribute_tag
+    echo "alert ANY ANY <> ANY ANY (msg:`"$Message`"; content:`"$Content`"; reference:$Reference; classtype:$Classtype; sid:$SID; rev:1;)" `n  >> $OutputFile`.rules
+    $global:SID += 1
+    $global:TotalHashes += 1
+   }
+    if ($FileContent[$Count].type -eq "hash_sha1"){
+    $Type = @()
+    $Content = @()
+    $Message = @()
+    $Type += $FileContent[$Count].type
+    $Content += $FileContent[$Count].value
+    $Message += $FileContent[$Count].type 
+    $Reference += $FileContent[$Count].attribute_tag
+    echo "alert ANY ANY <> ANY ANY (msg:`"$Message`"; content:`"$Content`"; reference:$Reference; classtype:$Classtype; sid:$SID; rev:1;)" `n  >> $OutputFile`.rules
+    $global:SID += 1
+    $global:TotalHashes += 1
+   }
+   if ($FileContent[$Count].type -eq "sha256"){
+    $Type = @()
+    $Content = @()
+    $Message = @()
+    $Type += $FileContent[$Count].type
+    $Content += $FileContent[$Count].value
+    $Message += $FileContent[$Count].type 
+    $Reference += $FileContent[$Count].attribute_tag
+    echo "alert ANY ANY <> ANY ANY (msg:`"$Message`"; content:`"$Content`"; reference:$Reference; classtype:$Classtype; sid:$SID; rev:1;)" `n  >> $OutputFile`.rules
+    $global:SID += 1
+    $global:TotalHashes += 1
+   }
+    if ($FileContent[$Count].type -eq "sha384"){
+    $Type = @()
+    $Content = @()
+    $Message = @()
+    $Type += $FileContent[$Count].type
+    $Content += $FileContent[$Count].value
+    $Message += $FileContent[$Count].type 
+    $Reference += $FileContent[$Count].attribute_tag
+    echo "alert ANY ANY <> ANY ANY (msg:`"$Message`"; content:`"$Content`"; reference:$Reference; classtype:$Classtype; sid:$SID; rev:1;)" `n  >> $OutputFile`.rules
+    $global:SID += 1
+    $global:TotalHashes += 1
+   }
+    if ($FileContent[$Count].type -eq "sha512"){
+    $Type = @()
+    $Content = @()
+    $Message = @()
+    $Type += $FileContent[$Count].type
+    $Content += $FileContent[$Count].value
+    $Message += $FileContent[$Count].type 
+    $Reference += $FileContent[$Count].attribute_tag
+    echo "alert ANY ANY <> ANY ANY (msg:`"$Message`"; content:`"$Content`"; reference:$Reference; classtype:$Classtype; sid:$SID; rev:1;)" `n  >> $OutputFile.rules
+    $global:SID += 1
+    $global:TotalHashes += 1
+   }
+    if ($FileContent[$Count].type -eq "authentihash"){
+    $Type = @()
+    $Content = @()
+    $Message = @()
+    $Type += $FileContent[$Count].type
+    $Content += $FileContent[$Count].value
+    $Message += $FileContent[$Count].type 
+    $Reference += $FileContent[$Count].attribute_tag
+    echo "alert ANY ANY <> ANY ANY (msg:`"$Message`"; content:`"$Content`"; reference:$Reference; classtype:$Classtype; sid:$SID; rev:1;)" `n  >> $OutputFile`.rules
+    $global:SID += 1
+    $global:TotalHashes += 1
+   }
+    if ($FileContent[$Count].type -eq "ssdeep"){
+    $Type = @()
+    $Content = @()
+    $Message = @()
+    $Type += $FileContent[$Count].type
+    $Content += $FileContent[$Count].value
+    $Message += $FileContent[$Count].type 
+    $Reference += $FileContent[$Count].attribute_tag
+    echo "alert ANY ANY <> ANY ANY (msg:`"$Message`"; content:`"$Content`"; reference:$Reference; classtype:$Classtype; sid:$SID; rev:1;)" `n  >> $OutputFile`.rules
+    $global:SID += 1
+    $global:TotalHashes += 1
     }
-    elseif ($FileExten -eq ".csv") {
-        write-host "CSV yes, please!!"
-        #Check-Headers
-    }
-    elseif ($FileExten -eq "") {
-        write-host "I did not detect a file extension..."
+    if ($FileContent[$Count].type -eq "md5"){
+        $Type = @()
+        $Content = @()
+        $Message = @()
+        $Type += $FileContent[$Count].type
+        $Content += $FileContent[$Count].value
+        $Message += $FileContent[$Count].type 
+        $Reference += $FileContent[$Count].attribute_tag
+        echo "alert ANY ANY <> ANY ANY (msg:`"$Message`"; content:`"$Content`"; reference:$Reference; classtype:$Classtype; sid:$SID; rev:1;)" `n  >> $OutputFile`.rules
+        $global:SID += 1
+        $global:TotalHashes += 1
     }
     else {
-        write-host "Something didn't work..."
+        $global:TotalSkips += 1
     }
 }
 
-Function Excel-File ($File) {
-    #Ensure the naming scheme is right...
-    $Excel = New-Object -ComObject Excel.Application
-    $wb = $Excel.Workbooks.Open($File)
-    $global:CSVFile=Join-Path -Path $FilePath -ChildPath $FileName".csv"
-    foreach ($ws in $wb.Worksheets) {
-        $ws.SaveAs($CSVFile, 6)
-    ###Use a Join-Path here if possible...
-       $File = $CSVFile
-      # write-host="$File"
-    }
-    $Excel.Quit()
-    #write-host "`$File = $File"
-    Detect-Filetype
+##Iterates through the file and writes pertinent info to suricata rules##
+while ($Count -lt $length){
+    Detect-Type
+    $Count += 1
+    Write-host $global:SID
 }
 
-Function Build-Results {
-    New-Item -Name "Results" -ItemType Directory -Path $PSScriptRoot -erroraction 'silentlycontinue'
-    #Edit permissions?
-    New-Item -Name "Readme.txt" -ItemType File -Path $ResultsPath
-    $TemplateContent = get-content -path $TempReadmePath
-    Invoke-expression """$TemplateContent""" | set-content -path $ReadmePath
-}
-#Detect-Filetype
 Build-Results
 write-host "done"
