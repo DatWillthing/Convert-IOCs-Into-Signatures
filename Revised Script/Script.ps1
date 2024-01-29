d5ec49fa5255cefb098d27fcc783f5b80c1f0d0a
DatWillthing <79482819+DatWillthing@users.noreply.github.com>
2024-01-29 08:55:08 -0800

    Add files via upload
    update as of 1/29/2024, does not include intelligent header detection.

---
+++ Revised Script/Script.ps1
@@ -0,0 +1,29 @@
+$csv = read-host "Input CSV Name"
+$mem = import-csv $csv
+
+$count = 0
+$sid = 30000001
+while ($count -lt $mem.count){
+    if ($mem[$count].type -eq "FQDN") {
+        $Value = $mem[$count].Value
+        add-content -path .\apt29.rules -value "alert ip any any <> any any (msg:`"$Value`";content:`"$Value`";rev:1;sid:$sid;)"
+    }
+    if ($mem[$count].type -eq "IPV4") {
+        $Value = $mem[$count].Value
+        add-content -path .\apt29.rules -value "alert ip $Value any <> any any (msg:`"$Value`";rev:1;sid:$sid;)"
+    }
+    ##MD5 Rule needs proper rule field
+    if ($mem[$count].type -eq "MD5") {
+        $Value = $mem[$count].Value
+        add-content -path .\apt29.rules -value "alert ip any any <> any any (msg:`"MD5: $Value`";rev:1;sid:$sid;)"
+    }
+     if ($mem[$count].type -eq "URL") {
+        $Initial = $mem[$count].Value
+        $Value = $Initial.replace(':','|3A|')
+        add-content -path .\apt29.rules -value "alert ip any any <> any any (msg:`"URL: $Value`";content:`"$Value`"rev:1; sid:$sid;)"
+    }
+    $count += 1
+    $sid += 1
+    #write-host $sid
+}
+exit
